2c7a03216d476b56e19c271897a9606cedd34831
gabriel <ga-yo-so@hotmail.com>
2016-05-08 23:58:01 -0300

    subo carga de archivos de configuracion. Esta todo tirado en el main, hay que separar la carga y el parque

--- gabo2/main.cpp
+++ gabo2/main.cpp
@@ -3,6 +3,8 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fstream>
+#include <sstream>
 #include "Juego.h"
 #include "Persona.h"
 #include "Semaforo.h"
@@ -17,39 +19,92 @@ int main()
 {
     Logger::log("MAIN", "Empezando simulacion", DEBUG);
 
+    /// ---------------------- formato de archivo de configuracion ----------------------
+
+    /*
+
+    periodo_chequeo_caja
+    cant_juegos
+    costo_juego_1 capacidad_juego_1
+    costo_juego_2 capacidad_juego_2
+    ...
+    cant_personas
+    plata_persona_1
+    plata_persona_2
+    ...
+
+    */
+
+    /// ---------------------------------------------------------------------------------
+
+    ifstream input_file;
+    input_file.open("config.txt", ifstream::in);
+    string temp;
+    stringstream ss;
+    string t;
+
+    // leo periodo chequeo caja
+    getline(input_file, temp);
+    int periodo_chequeo_caja = std::stoi(temp, 0, 10);
+
+    // leo cantidad juegos
+    getline(input_file, temp);
+    int cant_juegos = std::stoi(temp, 0, 10);
+
+    // leo costo y capacidad de cada juego
+    vector<int> costo_juegos;
+    vector<int> capacidad_juegos;
+
+    for(int i = 0; i < cant_juegos; ++i){
+        getline(input_file, temp);
+        ss << temp;
+
+        getline(ss, t, ' ');
+        costo_juegos.push_back(std::stoi(t, 0, 10));
+        getline(ss, t, ' ');
+        capacidad_juegos.push_back(std::stoi(t, 0, 10));
+    }
+
+    // leo cantidad personas
+    getline(input_file, temp);
+    int cant_personas = std::stoi(temp, 0, 10);
+
+    // leo plata personas
+    vector<int> plata_personas;
+    for(int i = 0; i < cant_personas; ++i){
+        getline(input_file, temp);
+        plata_personas.push_back(std::stoi(temp, 0, 10));
+    }
+
     // creo la caja
     Logger::log("MAIN", "Creo la caja", DEBUG);
     Caja* caja = Caja::getInstance();
     caja->crear();
 
     // creo al administrador
-    int periodo_chequeo_caja = 5;
     Logger::log("MAIN", "Creo al administrador", DEBUG);
     Administrador administrador(periodo_chequeo_caja);
     pid_t administrador_pid = administrador.run();
 
     // crear juegos
     Logger::log("MAIN", "Creo juegos", DEBUG);
-    int cant_juegos = 1; /// HARDCODEO
+    //int cant_juegos = 1; /// HARDCODEO
     vector<pid_t> juegos;
     vector<string> nom_juegos;
-    vector<int> costo_juegos;
     for(int i = 1; i <= cant_juegos; ++i){
         std::string nom_juego = "juego" + std::to_string(i);
         nom_juegos.push_back(nom_juego);
-        costo_juegos.push_back(5); /// costo esta hardcodeado aca
 
-        Juego juego(nom_juego, 2, costo_juegos[i-1]); ///capacidad esta hardcodeada aca
+        Juego juego(nom_juego, capacidad_juegos[i-1], costo_juegos[i-1]);
         pid_t juego_pid = juego.run();
         juegos.push_back(juego_pid);
     }
 
     // crear personas
     Logger::log("MAIN", "Creo personas", DEBUG);
-    int cant_personas = 2; /// HARDCODEO
     vector<pid_t> personas;
     for(int i = 0; i < cant_personas; ++i){
-        Persona persona(5); /// aca esta hardcodeada la plata
+        Persona persona(plata_personas[i]);
         persona.loadInfoParque(nom_juegos, costo_juegos);
 
         pid_t persona_pid = persona.run();
