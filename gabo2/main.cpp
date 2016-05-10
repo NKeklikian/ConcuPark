2faed5b031b8929630e19c1c5cb90dcf76813c33
gabriel <ga-yo-so@hotmail.com>
2016-05-09 22:29:45 -0300

    subo el main mas ordenados. no hice una clase parque al final, yo creo que asi ya se puede leer facil el main (con solo ver las llamadas al logger se ve todo lo que hace)

--- gabo2/main.cpp
+++ gabo2/main.cpp
@@ -14,11 +14,9 @@
 
 using namespace std;
 
-// esto vendria a ser el parque, despues se podria abstraer a otra clase runnable
-int main()
-{
-    Logger::log("MAIN", "Empezando simulacion", DEBUG);
-
+void cargarArchivoConfiguracion(int &periodo_chequeo_caja, int &cant_juegos,
+                                vector<int> &costo_juegos, vector<int> &capacidad_juegos,
+                                int &cant_personas, vector<int> &plata_personas){
     /// ---------------------- formato de archivo de configuracion ----------------------
 
     /*
@@ -45,16 +43,13 @@ int main()
 
     // leo periodo chequeo caja
     getline(input_file, temp);
-    int periodo_chequeo_caja = std::stoi(temp, 0, 10);
+    periodo_chequeo_caja = std::stoi(temp, 0, 10);
 
     // leo cantidad juegos
     getline(input_file, temp);
-    int cant_juegos = std::stoi(temp, 0, 10);
+    cant_juegos = std::stoi(temp, 0, 10);
 
     // leo costo y capacidad de cada juego
-    vector<int> costo_juegos;
-    vector<int> capacidad_juegos;
-
     for(int i = 0; i < cant_juegos; ++i){
         getline(input_file, temp);
         ss << temp;
@@ -67,30 +62,16 @@ int main()
 
     // leo cantidad personas
     getline(input_file, temp);
-    int cant_personas = std::stoi(temp, 0, 10);
+    cant_personas = std::stoi(temp, 0, 10);
 
     // leo plata personas
-    vector<int> plata_personas;
     for(int i = 0; i < cant_personas; ++i){
         getline(input_file, temp);
         plata_personas.push_back(std::stoi(temp, 0, 10));
     }
+}
 
-    // creo la caja
-    Logger::log("MAIN", "Creo la caja", DEBUG);
-    Caja* caja = Caja::getInstance();
-    caja->crear();
-
-    // creo al administrador
-    Logger::log("MAIN", "Creo al administrador", DEBUG);
-    Administrador administrador(periodo_chequeo_caja);
-    pid_t administrador_pid = administrador.run();
-
-    // crear juegos
-    Logger::log("MAIN", "Creo juegos", DEBUG);
-    //int cant_juegos = 1; /// HARDCODEO
-    vector<pid_t> juegos;
-    vector<string> nom_juegos;
+void crearJuegos(int cant_juegos, vector<pid_t> &juegos, vector<string> &nom_juegos, vector<int> capacidad_juegos, vector<int> costo_juegos){
     for(int i = 1; i <= cant_juegos; ++i){
         std::string nom_juego = "juego" + std::to_string(i);
         nom_juegos.push_back(nom_juego);
@@ -99,10 +80,9 @@ int main()
         pid_t juego_pid = juego.run();
         juegos.push_back(juego_pid);
     }
+}
 
-    // crear personas
-    Logger::log("MAIN", "Creo personas", DEBUG);
-    vector<pid_t> personas;
+void crearPersonas(int cant_personas, vector<string> nom_juegos, vector<int> costo_juegos, vector<int> plata_personas, vector<pid_t> &personas){
     for(int i = 0; i < cant_personas; ++i){
         Persona persona(plata_personas[i]);
         persona.loadInfoParque(nom_juegos, costo_juegos);
@@ -110,29 +90,66 @@ int main()
         pid_t persona_pid = persona.run();
         personas.push_back(persona_pid);
     }
+}
 
-    // espero a que se vayan todas las personas
+void esperarQueSeVayanLasPersonas(vector<pid_t> &personas){
     int status;
     while(personas.size() > 0){
         waitpid(personas[personas.size()-1], &status, 0);
         personas.pop_back();
     }
-    Logger::log("MAIN", "Se fueron todos", DEBUG);
+}
 
-    // cierro los juegos
-    Logger::log("MAIN", "Cierro los juegos", DEBUG);
+void cerrarJuegos(vector<pid_t> juegos){
+    int status;
     while(juegos.size() > 0){
         kill(juegos[juegos.size()-1], SIGINT);
         waitpid(juegos[juegos.size()-1], &status, 0);
         juegos.pop_back();
     }
+}
 
-    // cierro al administrador
+// esto vendria a ser el parque, despues se podria abstraer a otra clase runnable
+int main()
+{
+    int periodo_chequeo_caja;
+    int cant_juegos;
+    vector<int> costo_juegos;
+    vector<int> capacidad_juegos;
+    int cant_personas;
+    vector<int> plata_personas;
+    cargarArchivoConfiguracion(periodo_chequeo_caja, cant_juegos, costo_juegos, capacidad_juegos, cant_personas, plata_personas);
+
+    Logger::log("MAIN", "Empezando simulacion", DEBUG);
+
+    Logger::log("MAIN", "Creo la caja", DEBUG);
+    Caja* caja = Caja::getInstance();
+    caja->crear();
+
+    Logger::log("MAIN", "Creo al administrador", DEBUG);
+    Administrador administrador(periodo_chequeo_caja);
+    pid_t administrador_pid = administrador.run();
+
+    Logger::log("MAIN", "Creo juegos", DEBUG);
+    vector<pid_t> juegos;
+    vector<string> nom_juegos;
+    crearJuegos(cant_juegos, juegos, nom_juegos, capacidad_juegos, costo_juegos);
+
+    Logger::log("MAIN", "Creo personas", DEBUG);
+    vector<pid_t> personas;
+    crearPersonas(cant_personas, nom_juegos, costo_juegos, plata_personas, personas);
+
+    int status;
+    esperarQueSeVayanLasPersonas(personas);
+    Logger::log("MAIN", "Se fueron todos", DEBUG);
+
+    Logger::log("MAIN", "Cierro los juegos", DEBUG);
+    cerrarJuegos(juegos);
+
     Logger::log("MAIN", "Cierro al administrador", DEBUG);
     kill(administrador_pid, SIGINT);
     waitpid(administrador_pid, &status, 0);
 
-    // cierro la caja
     Logger::log("MAIN", "Cierro la caja", DEBUG);
     caja->eliminar();
 
